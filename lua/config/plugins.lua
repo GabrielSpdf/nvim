220f1ae2988270dcae4e3c92eb8b3722dade1a5a
GabrielSpdf <gb.spada@hotmail.com>
2025-01-02 00:06:44 -0300

    adding new features

--- lua/config/plugins.lua
+++ lua/config/plugins.lua
@@ -1,65 +1,65 @@
 -- plugins.lua
 require('packer').startup(function(use)
 	use 'wbthomason/packer.nvim'
-	use({
-		"rose-pine/neovim",
-		as = "rose-pine",
-		config = function()
-			require("rose-pine").setup({
-				variant = "main",      -- auto, main, moon, or dawn
-				dark_variant = "main", -- main, moon, or dawn
-				-- dim_inactive_windows = false,
-				-- extend_background_behind_borders = true,
-				bold_vert_split = false,
-				dim_nc_background = false,
-				disable_background = false,
-				disable_float_background = false,
-				disable_italics = false,
+	-- use({
+	-- 	"rose-pine/neovim",
+	-- 	as = "rose-pine",
+	-- 	config = function()
+	-- 		require("rose-pine").setup({
+	-- 			variant = "main",      -- auto, main, moon, or dawn
+	-- 			dark_variant = "main", -- main, moon, or dawn
+	-- 			-- dim_inactive_windows = false,
+	-- 			-- extend_background_behind_borders = true,
+	-- 			bold_vert_split = false,
+	-- 			dim_nc_background = false,
+	-- 			disable_background = false,
+	-- 			disable_float_background = false,
+	-- 			disable_italics = false,
 
-				enable = {
-				  terminal = true,
-				  legacy_highlights = true,
-				  migrations = true,
-				},
+	-- 			enable = {
+	-- 			  terminal = true,
+	-- 			  legacy_highlights = true,
+	-- 			  migrations = true,
+	-- 			},
 
 
-				groups = {
-				  border = "muted",
-				  link = "iris",
-				  panel = "surface",
+	-- 			groups = {
+	-- 			  border = "muted",
+	-- 			  link = "iris",
+	-- 			  panel = "surface",
 
-				  error = "love",
-				  hint = "iris",
-				  info = "foam",
-				  note = "pine",
-				  todo = "rose",
-				  warn = "gold",
+	-- 			  error = "love",
+	-- 			  hint = "iris",
+	-- 			  info = "foam",
+	-- 			  note = "pine",
+	-- 			  todo = "rose",
+	-- 			  warn = "gold",
 
-				  git_add = "foam",
-				  git_change = "rose",
-				  git_delete = "love",
-				  git_dirty = "rose",
-				  git_ignore = "muted",
-				  git_merge = "iris",
-				  git_rename = "pine",
-				  git_stage = "iris",
-				  git_text = "rose",
-				  git_untracked = "subtle",
+	-- 			  git_add = "foam",
+	-- 			  git_change = "rose",
+	-- 			  git_delete = "love",
+	-- 			  git_dirty = "rose",
+	-- 			  git_ignore = "muted",
+	-- 			  git_merge = "iris",
+	-- 			  git_rename = "pine",
+	-- 			  git_stage = "iris",
+	-- 			  git_text = "rose",
+	-- 			  git_untracked = "subtle",
 
-				  h1 = "iris",
-				  h2 = "foam",
-				  h3 = "rose",
-				  h4 = "gold",
-				  h5 = "pine",
-				  h6 = "foam",
-				},
+	-- 			  h1 = "iris",
+	-- 			  h2 = "foam",
+	-- 			  h3 = "rose",
+	-- 			  h4 = "gold",
+	-- 			  h5 = "pine",
+	-- 			  h6 = "foam",
+	-- 			},
 
-				before_highlight = function(group, highlight, palette)
-				end,
-			  })
-			vim.cmd("colorscheme rose-pine")
-		end,
-	})
+	-- 			before_highlight = function(group, highlight, palette)
+	-- 			end,
+	-- 		  })
+	-- 		vim.cmd("colorscheme rose-pine")
+	-- 	end,
+	-- })
 	use 'tpope/vim-surround'
 	use 'tpope/vim-fugitive'
 	use 'preservim/nerdtree'
@@ -137,9 +137,46 @@ require('packer').startup(function(use)
 
 	use 'romgrk/barbar.nvim'
 	use 'neovim/nvim-lspconfig'
+	use 'rebelot/kanagawa.nvim'
+	use 'marko-cerovac/material.nvim'
+	use 'aktersnurra/no-clown-fiesta.nvim'
 	use {
 		"akinsho/toggleterm.nvim", 
 		tag = '*', 
 		-- config = function()
 	}
+   -- use {
+   --    "folke/noice.nvim",
+   --    requires = {
+   --      "MunifTanjim/nui.nvim",
+   --    }
+   --  }
+	-- require("noice").setup({
+	   -- lsp = {
+		   -- override = {
+		   -- ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+		   -- ["vim.lsp.util.stylize_markdown"] = true,
+			-- ["cmp.entry.get_documentation"] = true,
+		   -- }
+	   -- },
+	  
+	   -- -- you can enable a preset for easier configuration
+	   -- presets = {
+		   -- bottom_search = true, -- use a classic bottom cmdline for search
+		   -- command_palette = true, -- position command palette in the center
+		   -- long_message_to_split = true, -- long messages will be sent to a split
+	   -- },
+	   -- -- set up a pretty border for the popup windows
+	   -- popupmenu = {
+		   -- backend = "nui",
+		   -- border = "rounded",
+	   -- },
+	-- })
+	require'barbar'.setup {
+		animation = true,
+		auto_hide = false,
+		tabpages = true,
+		clickable = true,
+	}
+	vim.cmd("colorscheme material-deep-ocean")
 end)
